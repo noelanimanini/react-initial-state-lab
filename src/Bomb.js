d1075b596f2a018bd22b14d3466465ddc3204399
noelanimanini <noelani26@crystals-MacBook-Pro.local>
2021-02-02 10:49:56 -0800

    I dont need to string interpolate. The react page already renders it correctly

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,22 @@
-// your Bomb code here!
+// your Bomb code her
+import React from 'react'
+
+export default class Bomb extends React.Component {
+    constructor(props) {
+        super() 
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render () {
+
+        if (this.state.secondsLeft === 0) {return(
+            <h1>Boom!</h1>
+        )} else {
+            return (<h2>
+            {this.state.secondsLeft} seconds before I go boom!
+            </h2>)}
+       
+    }
+}
